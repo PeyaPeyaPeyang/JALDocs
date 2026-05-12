7b2e36b5841c4eae7d351a8b827ce9f05d5de6a8
PeyaPeyaPeyang <peyang@peya.tokyo>
2026-05-12 18:42:31 +0900

    refactor: Remove legacy components from HomepageHeader and replace with HomepageRetroHero

--- lang-jal/src/pages/index.tsx
+++ lang-jal/src/pages/index.tsx
@@ -3,29 +3,21 @@ import Link from '@docusaurus/Link';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
 import type {IconDefinition} from '@fortawesome/fontawesome-svg-core';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {
-  faArrowDown,
   faArrowRight,
-  faBookOpen,
   faBolt,
-  faCircleQuestion,
-  faCodeBranch,
   faCompassDrafting,
-  faCube,
-  faDownload,
   faFileCode,
   faGear,
-  faGraduationCap,
   faLayerGroup,
   faMagnifyingGlass,
-  faPlay,
   faTerminal,
   faWandMagicSparkles,
 } from '@fortawesome/free-solid-svg-icons';
 import {faGithub, faJava} from '@fortawesome/free-brands-svg-icons';
+import HomepageRetroHero from '@site/src/components/HomepageRetroHero';
 import InstructionTrace from '@site/src/components/InstructionTrace';
 
 import styles from './index.module.css';
@@ -97,15 +89,6 @@ const metricItems = [
   {label: 'IntelliJ', detail: 'editor support', icon: faBolt},
 ];
 
-const legacyTiles = [
-  {label: 'Go JAL', icon: faPlay},
-  {label: 'JAL + IntelliJ', icon: faBolt},
-  {label: 'StackMapFrame', icon: faLayerGroup},
-  {label: 'Bytecode One', icon: faCube},
-  {label: 'JAL Academy', icon: faGraduationCap},
-  {label: 'JAL Magazine', icon: faBookOpen},
-];
-
 const toolingItems = [
   {label: 'ツール導入', href: '/docs/usage/tooling', icon: faWandMagicSparkles, internal: true},
   {label: 'Javasm Plugin', href: 'https://plugins.jetbrains.com/plugin/27944-javasm', icon: faBolt},
@@ -130,111 +113,9 @@ function IconLabel({
 }
 
 function HomepageHeader(): ReactNode {
-  const logoUrl = useBaseUrl('/img/jal-logo.svg');
-
   return (
     <header className={styles.hero}>
-      <div className={styles.retroFrame}>
-        <div className={styles.retroBar}>
-          <div className={styles.retroBarInner}>
-            <div className={styles.retroBrand}>
-              <img className={styles.logoMark} src={logoUrl} alt="" />
-              <span>Jal<span className={styles.trademark}>™</span></span>
-            </div>
-            <nav className={styles.retroNav} aria-label="Primary">
-              <Link to="/docs/intro">
-                <IconLabel className={styles.navLabel} icon={faDownload}>
-                  ダウンロード
-                </IconLabel>
-              </Link>
-              <Link to="/docs/language-guide/basics/syntax">
-                <IconLabel className={styles.navLabel} icon={faCircleQuestion}>
-                  ヘルプ
-                </IconLabel>
-              </Link>
-            </nav>
-            <label className={styles.retroSearch}>
-              <span>
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-                <span>検索</span>
-              </span>
-              <input aria-label="検索" />
-            </label>
-          </div>
-        </div>
-        <div className={styles.heroInner}>
-          <section className={styles.heroCopy}>
-            <Heading as="h1" className={styles.heroTitle}>
-              あなたとJAL,
-              <br />
-              今すぐアセンブ
-              <br />
-              ル
-            </Heading>
-            <div className={styles.actions}>
-              <Link className={styles.downloadButton} to="/docs/intro">
-                <IconLabel className={styles.buttonLabel} icon={faDownload}>
-                  無料Jalのダウンロード
-                </IconLabel>
-              </Link>
-            </div>
-            <div className={styles.legacyLinks}>
-              <Link to="/docs/language-guide/basics/syntax">
-                <IconLabel className={styles.inlineLinkLabel} icon={faBookOpen}>
-                  JALとは
-                </IconLabel>
-              </Link>
-              <Link to="/docs/usage/tooling">
-                <IconLabel className={styles.inlineLinkLabel} icon={faTerminal}>
-                  JALの有無のチェック
-                </IconLabel>
-              </Link>
-              <Link to="/docs/language-guide/runtime/instructions">
-                <IconLabel className={styles.inlineLinkLabel} icon={faCodeBranch}>
-                  サポート情報
-                </IconLabel>
-              </Link>
-            </div>
-          </section>
-          <aside className={styles.developerBox}>
-            <strong>
-              <IconLabel className={styles.developerHeading} icon={faTerminal}>
-                開発者向け
-              </IconLabel>
-            </strong>
-            <Link to="/docs/usage/tooling">
-              <IconLabel className={styles.developerLinkLabel} icon={faGear}>
-                JAL CLI / Gradle / Javasm
-              </IconLabel>
-            </Link>
-            <Link to="/docs/language-guide/runtime/instructions">
-              <IconLabel className={styles.developerLinkLabel} icon={faCodeBranch}>
-                命令リファレンス
-              </IconLabel>
-            </Link>
-          </aside>
-        </div>
-        <div className={styles.heroBottom}>
-          <strong>JALについて</strong>
-          <div className={styles.tileRow}>
-            {legacyTiles.map((tile) => (
-              <div className={styles.legacyTile} key={tile.label}>
-                <FontAwesomeIcon className={styles.tileIcon} icon={tile.icon} />
-                <span>{tile.label}</span>
-              </div>
-            ))}
-          </div>
-        </div>
-        <a className={styles.scrollPrompt} href="#overview">
-          <span>さらに詳しく</span>
-          <span aria-hidden="true">
-            <FontAwesomeIcon icon={faArrowDown} />
-          </span>
-        </a>
-        <div className={styles.oracleLikeLogo} aria-label="LANGJAL">
-          LANGJAL
-        </div>
-      </div>
+      <HomepageRetroHero />
     </header>
   );
 }
