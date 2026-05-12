2a30d72f617fa147446d15531c64270f25fb3faf
PeyaPeyaPeyang <peyang@peya.tokyo>
2026-05-12 21:13:02 +0900

    chore: Add deploy settings

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,15 +19,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://peyapeyapeyang.github.io',
+  url: 'https://jal.peya.tokyo',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/JVMAssemblyLanguage/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'PeyaPeyaPeyang',
-  projectName: 'JVMAssemblyLanguage',
+  projectName: 'JALDocs',
 
   onBrokenLinks: 'throw',
 
